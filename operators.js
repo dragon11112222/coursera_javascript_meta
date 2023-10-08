5c4c30f8c18723be4e51a5301c82ad9b0f3decc0
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-08 11:53:37 +0900

    연산자
    - 논리연산자 && || 
    - 산술연산자 % +
    - 숫자 + 문자 = 문자

---
+++ operators.js
@@ -0,0 +1,33 @@
+// logical and
+var score = 8;
+console.log('Mid-level skills:', score > 0 && score < 10)
+
+// logical or
+var timeRemaining = 0;
+var energy = 10;
+console.log('Game over: ', timeRemaining == 0 || energy == 0)
+
+// modulus
+var num1 = 2;
+var num2 = 5;
+var test1 = num1 % 2;
+var test2 = num2 % 2;
+var result1 = test1 == 0;
+var result2 = test2 == 0;
+console.log('Is', num1, "an even number?", result1);
+console.log("Is", num2, "an even number?", result2)
+
+// string concatenate
+var now = 'Now in ';
+var three = 3;
+var d = 'D!';
+console.log(now + three + d);
+
+// number accumulate
+var counter = 0;
+counter += 5;
+counter += 3;
+console.log(counter);
+
+
+console.log(1 + '1');
