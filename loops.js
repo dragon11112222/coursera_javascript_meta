7233fa8b94134a30749fb964462f7b995c43199e
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-10 23:56:10 +0900

    반복문
    - for
    - while

---
+++ loops.js
@@ -0,0 +1,29 @@
+for (var i = 1; i < 6; i ++) {
+    console.log(i);
+}
+console.log('Counting completed!')
+
+for (var i = 5; i > 0; i --) {
+    console.log(i);
+}
+console.log('Countdown finished!')
+
+var counter = 1
+while (counter < 6) {
+    console.log(counter);
+    counter ++;
+}
+console.log('Counting completed!')
+
+var counter = 5;
+while (counter > 0) {
+    console.log(counter);
+    counter --;
+}
+console.log('Countdown finished!')
+
+var year = 2018;
+while (year < 2023) {
+    console.log(year);
+    year ++;
+}
