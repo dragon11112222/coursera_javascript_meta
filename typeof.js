7c9b947e34308b0a918c5eb492e341c3fe3ceb09
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-15 21:18:52 +0900

    타입 출력
    - 파이썬과는 달리 정수와 실수(float)를 구분하지 않고 다 number로 취급.
    - array[]는 object{}로 취급.

---
+++ typeof.js
@@ -0,0 +1,14 @@
+var test = typeof(10);
+console.log(test);
+
+var test1 = typeof(3.14);
+console.log(test1);
+
+var test2 = typeof(1 < 2);
+console.log(test2);
+
+var test3 = typeof([1, 2, 3]);
+console.log(test3);
+
+var test4 = typeof({});
+console.log(test4);
