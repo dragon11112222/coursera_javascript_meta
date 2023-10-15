662a78c1d67726c2f34f8292f91329ec8c815256
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-15 20:54:21 +0900

    array & object
    array는 [] 안에다 값만,
    object는 {} 안에다 property key와 property value로 구성.

---
+++ arrayObjects.js
@@ -0,0 +1,14 @@
+var clothes = [];
+clothes.push('jumper');
+clothes.push('cap');
+clothes.push('sneakers');
+clothes.push('pants');
+clothes.push('skirt');
+clothes.pop('skirt');
+clothes.push('hood');
+console.log(clothes);
+
+var favCar = {};
+favCar.color = 'skyblue';
+favCar.convertible = true;
+console.log(favCar);
