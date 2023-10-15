ff98674972307237d4e012a75a6aa4c590728da5
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-15 21:07:08 +0900

    object 안에 property value로 함수가 올 때
    - object 전체를 console.log() 하면 함수가 실행되지 않고,
    - object의 dot notation을 적용했을 때 함수가 실행된다.

--- arrayObjects.js
+++ arrayObjects.js
@@ -12,3 +12,18 @@ var favCar = {};
 favCar.color = 'skyblue';
 favCar.convertible = true;
 console.log(favCar);
+
+//example of adding properties and methods to an object
+var car = {};
+car.mileage = 98765;
+car.color = "red";
+console.log(car);
+car.turnTheKey = function() {
+    console.log("The engine is running")
+}
+car.lightsOn = function() {
+    console.log("The lights are on.")
+}
+console.log(car);
+car.turnTheKey();
+car.lightsOn()
