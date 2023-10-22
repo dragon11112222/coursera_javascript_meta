d8fee6cb6184d0914036922aabb0cb1cd18f3cab
Seyong JUNG, Ernesto <40597818+dragon11112222@users.noreply.github.com>
2023-10-22 21:29:42 +0900

    defensive programming
    condition1, condition2 설정 후 둘 다 true 일 경우에만 원래 코드 작동.

--- errors.js
+++ errors.js
@@ -15,3 +15,27 @@ function addTwoNums(a, b) {
 
 addTwoNums(5, "5");
 console.log("It still works");
+
+
+
+// defensive programming
+function letterFinder(word, match) {
+    var condition1 = typeof(word) == 'string' && word.length >= 2;
+    var condition2 = typeof(match) == 'string' && match.length == 1;
+    if(condition1 == true && condition2 == true) {
+        for(var i = 0; i < word.length; i++) {
+            if(word[i] == match) {
+                //if the current character at position i in the word is equal to the match
+                console.log('Found the', match, 'at', i)
+            } else {
+                console.log('---No match found at', i)
+            }
+        }
+    } else {
+        console.log("Please pass correct arguments to the function.");
+    }
+
+}
+
+letterFinder(10, 0);
+letterFinder("cat", "c");
